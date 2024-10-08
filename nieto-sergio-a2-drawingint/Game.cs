cb6fe1f4b34cb6699f5df4633d112f56dd99111e
Shaft0509 <sergionieto555@gmail.com>
2024-10-08 17:53:29 -0500

    eliminated initial comments

---
+++ nieto-sergio-a2-drawingint/Game.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Numerics;
+
+
+namespace Game10003
+{
+
+    public class Game
+    {
+     
+        public void Setup()
+        {
+
+        }
+
+
+        public void Update()
+        {
+
+        }
+    }
+}
