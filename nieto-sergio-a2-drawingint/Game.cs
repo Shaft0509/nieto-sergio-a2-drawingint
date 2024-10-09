595adc514de657d17d841adc35eada5eaeaa022c
Shaft0509 <sergionieto555@gmail.com>
2024-10-08 23:32:52 -0500

    Added triangle

--- nieto-sergio-a2-drawingint/Game.cs
+++ nieto-sergio-a2-drawingint/Game.cs
@@ -9,11 +9,15 @@ namespace Game10003
 
     public class Game
     {
-     
+           
+
+
         public void Setup()
         {
             Window.SetTitle("Cotidianity Changes");
             Window.SetSize(800, 600);
+
+
         }
 
 
@@ -21,6 +25,9 @@ namespace Game10003
         {
                 Window.ClearBackground(Color.Blue);
          
+
+            // Draw rectangles
+
             Draw.FillColor = Color.Green;
             Draw.Rectangle(0, 450, 800, 150);
             
@@ -36,6 +43,24 @@ namespace Game10003
 
             Draw.FillColor = Color.Black;
             Draw.Rectangle(300, 450, 150, 130);
+           
+            
+            //Draw Triangle
+           
+            Draw.FillColor = Color.LightGray;
+
+            float x1 = 150;
+            float y1 = 300;
+            float x2 = 350;
+            float y2 = 100;
+            float x3 = 600;
+            float y3 = 300;
+
+            Draw.Triangle(150, 300, 375, 100, 605, 300);
+            
+          
+
+
 
                        
  
