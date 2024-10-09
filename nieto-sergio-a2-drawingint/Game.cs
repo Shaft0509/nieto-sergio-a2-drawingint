63eddf1428eaeaa5ca7a7ed4f8b924fc7a8f8ae1
Shaft0509 <sergionieto555@gmail.com>
2024-10-08 19:32:57 -0500

    Added Rectangles.

--- nieto-sergio-a2-drawingint/Game.cs
+++ nieto-sergio-a2-drawingint/Game.cs
@@ -20,7 +20,25 @@ namespace Game10003
         public void Update()
         {
                 Window.ClearBackground(Color.Blue);
+         
+            Draw.FillColor = Color.Green;
+            Draw.Rectangle(0, 450, 800, 150);
+            
+          
+            Draw.FillColor = Color.Red;
+            Draw.Rectangle(200, 300, 350, 280);
 
+            Draw.FillColor = Color.White;
+            Draw.Rectangle(250, 375, 100, 50);
+
+            Draw.FillColor = Color.White;
+            Draw.Rectangle(400, 375, 100, 50);
+
+            Draw.FillColor = Color.Black;
+            Draw.Rectangle(300, 450, 150, 130);
+
+                       
+ 
 
         }
     }
