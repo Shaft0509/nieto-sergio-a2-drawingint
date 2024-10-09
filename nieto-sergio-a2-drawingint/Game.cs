2e95b2a3040d25d656ddef84367869c4b1d6f13d
Shaft0509 <sergionieto555@gmail.com>
2024-10-09 02:18:30 -0500

    Added time elapsed, bool (true isday, fales isnight), x circle pos, speed of the circle and the color of the rectangles in order to call them later as an array.

--- nieto-sergio-a2-drawingint/Game.cs
+++ nieto-sergio-a2-drawingint/Game.cs
@@ -9,9 +9,26 @@ namespace Game10003
 
     public class Game
     {
-           
 
 
+        // Keeps track of time for day/night switching
+        private double timeElapsed = 0;
+
+        // True if it's day, false if it's night
+        private bool isDay = true;
+
+        // X position of the circle
+        private float circleX = 650;
+
+        // Speed of the circle moving left
+        private const float circleSpeed = 100;
+
+        // Colors for the rectangles
+        private Color[] rectColors = { Color.Green, Color.Red, Color.White, Color.White, Color.Black };
+      
+        
+        
+      
         public void Setup()
         {
             Window.SetTitle("Cotidianity Changes");
