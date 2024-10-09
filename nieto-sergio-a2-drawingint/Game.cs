e7d0d1b1c811cbc22b446065662cef72873a9b40
Shaft0509 <sergionieto555@gmail.com>
2024-10-09 02:02:28 -0500

    eliminated the first rectangles because i needed to add them and recall them as an array, also eliminated the triangle for it not to interfere with the coding,

--- nieto-sergio-a2-drawingint/Game.cs
+++ nieto-sergio-a2-drawingint/Game.cs
@@ -23,40 +23,6 @@ namespace Game10003
 
         public void Update()
         {
-                Window.ClearBackground(Color.Blue);
-         
-
-            // Draw rectangles
-
-            Draw.FillColor = Color.Green;
-            Draw.Rectangle(0, 450, 800, 150);
-            
-          
-            Draw.FillColor = Color.Red;
-            Draw.Rectangle(200, 300, 350, 280);
-
-            Draw.FillColor = Color.White;
-            Draw.Rectangle(250, 375, 100, 50);
-
-            Draw.FillColor = Color.White;
-            Draw.Rectangle(400, 375, 100, 50);
-
-            Draw.FillColor = Color.Black;
-            Draw.Rectangle(300, 450, 150, 130);
-           
-            
-            //Draw Triangle
-           
-            Draw.FillColor = Color.LightGray;
-
-            float x1 = 150;
-            float y1 = 300;
-            float x2 = 350;
-            float y2 = 100;
-            float x3 = 600;
-            float y3 = 300;
-
-            Draw.Triangle(150, 300, 375, 100, 605, 300);
             
           
 
